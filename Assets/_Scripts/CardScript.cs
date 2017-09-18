a2c8eec46209e1af489b1f935c99ff737f3a1493
glaif <smaldone@gmail.com>
2017-09-17 21:57:45 -0400

    Added card enlargement on shift-mouseover

--- Assets/_Scripts/CardScript.cs
+++ Assets/_Scripts/CardScript.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class CardScript : MonoBehaviour {
 
     public Card Card { get; set; }
+    bool shiftOn;
+    bool enlargedCard;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    void Start () {
+        shiftOn = false;
+        enlargedCard = false;
+    }
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            shiftOn = true;
+        } else {
+            shiftOn = false;
+        }
+    }
 
     public void OnMouseDown() {
-        Debug.Log("Clicked on card in " + this.transform.parent.name + " zone.");
-        this.transform.parent.SendMessage("OnMouseDown", null, SendMessageOptions.DontRequireReceiver);
+        if (enlargedCard == false) {
+            Debug.Log("Clicked on card in " + this.transform.parent.name + " zone.");
+            this.transform.parent.SendMessage("OnMouseDown", null, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    public void OnMouseOver() {
+        if (shiftOn && enlargedCard == false) {
+            this.transform.localScale = new Vector3(4f, 4f, 0f);
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, 1.5f, -1f);
+            enlargedCard = true;
+        } else if (shiftOn == false && enlargedCard == true) {
+            this.transform.localScale = new Vector3(1, 1, 1);
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, 0f, 0);
+            enlargedCard = false;
+        }
+    }
+
+    public void OnMouseExit() {
+        if (enlargedCard == true) {
+            this.transform.localScale = new Vector3(1, 1, 1);
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x, 0f, 0);
+            enlargedCard = false;
+        }
     }
 }
