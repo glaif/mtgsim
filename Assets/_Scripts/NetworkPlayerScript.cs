20d897a52c286bbf5527d5e6a29cb6ce19830095
glaif <smaldone@gmail.com>
2017-11-18 22:44:12 -0500

    Reverting some of the UNET changes in prep for PUN
    -Going to use PUN instead of UNET, as it seems to fit better

--- Assets/_Scripts/NetworkPlayerScript.cs
+++ Assets/_Scripts/NetworkPlayerScript.cs
@@ -1,56 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class NetworkPlayerScript : NetworkBehaviour {
+public class NetworkPlayerScript : MonoBehaviour {
     private GameObject localPlayerGO;
     private PlayerScript playerSC;
     private GameObject dsGO;
 
+    //[SyncVar]
+    //int testVar = 0;
+
     // Use this for initialization
     void Start () {
-		if (isLocalPlayer) {
-            MainGameScript mainGameSC = GameObject.Find("Battleground").GetComponent<MainGameScript>();
-            if (mainGameSC == null) {
-                Debug.Log("Null MainGameScript object reference for NetworkPlayerScript");
-            }
+		//if (isLocalPlayer) {
+  //          MainGameScript mainGameSC = GameObject.Find("Battleground").GetComponent<MainGameScript>();
+  //          if (mainGameSC == null) {
+  //              Debug.Log("Null MainGameScript object reference for NetworkPlayerScript");
+  //          }
 
-            // Is there a possible race condition here?
-            // Can LocalPlayer be null if mainGameSC is behind
-            // this script?
-            localPlayerGO = mainGameSC.LocalPlayerGO;
-            if (localPlayerGO == null) {
-                Debug.Log("Null localPlayer in SelectDeckClick");
-                Debug.LogError("Error getting localPlayer from NetworkPlayerScript");
-                return;
-            }
-            playerSC = localPlayerGO.GetComponent<PlayerScript>();
-            if (playerSC == null) {
-                Debug.LogError("Unable to find Player SC in NetworkPlayerScript");
-                return;
-            }
-            Debug.Log("Local Player Start Complete");
-        }
+  //          // Is there a possible race condition here?
+  //          // Can LocalPlayer be null if mainGameSC is behind
+  //          // this script?
+  //          localPlayerGO = mainGameSC.LocalPlayerGO;
+  //          if (localPlayerGO == null) {
+  //              Debug.Log("Null localPlayer in SelectDeckClick");
+  //              Debug.LogError("Error getting localPlayer from NetworkPlayerScript");
+  //              return;
+  //          }
+  //          playerSC = localPlayerGO.GetComponent<PlayerScript>();
+  //          if (playerSC == null) {
+  //              Debug.LogError("Unable to find Player SC in NetworkPlayerScript");
+  //              return;
+  //          }
+  //          Debug.Log("Local Player Start Complete");
+  //      }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //if (!isLocalPlayer)
+        //    return;
 
-    public override void OnStartClient() {
-        Debug.Log("OnStartClient fired");
+        //if (Input.GetKeyDown(KeyCode.Space)) {
+        //    Debug.Log("testVar: " + testVar);
+        //    CmdIncTest();
+        //    Debug.Log("testVar: " + testVar);
+        //}
     }
 
-    public override void OnStartLocalPlayer() {
-        Debug.Log("OnStartLocalPlayer fired");
-        if (dsGO == null) {
-            dsGO = UIGORegistry.Find("DeckSelectMenu");
-            if (dsGO == null) {
-                Debug.Log("Null GameObject reference for DeckSelectMenu");
-            }
-        }
-        dsGO.SetActive(true);
-    }
+    //[Command]
+    //void CmdIncTest() {
+    //    testVar += 1;
+    //}
+
+    //public override void OnStartClient() {
+    //    Debug.Log("OnStartClient fired");
+    //}
+
+    //public override void OnStartLocalPlayer() {
+    //    Debug.Log("OnStartLocalPlayer fired");
+    //    if (dsGO == null) {
+    //        dsGO = UIGORegistry.Find("DeckSelectMenu");
+    //        if (dsGO == null) {
+    //            Debug.Log("Null GameObject reference for DeckSelectMenu");
+    //        }
+    //    }
+    //    dsGO.SetActive(true);
+    //}
 }
