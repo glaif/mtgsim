a1e989aa22e0347d4a9770b44eb8c5217c20943e
glaif <smaldone@gmail.com>
2017-11-22 16:13:12 -0500

    Some small mods for testing purposes

--- Assets/_Scripts/NetworkPlayerScript.cs
+++ Assets/_Scripts/NetworkPlayerScript.cs
@@ -4,9 +4,12 @@ public class NetworkPlayerScript : Photon.MonoBehaviour/*, IPunObservable*/ {
     // The PUN loglevel
     public PhotonLogLevel Loglevel = PhotonLogLevel.Informational;
 
+    private int msgNum = 0;
+
     void Start() {
         Debug.Log("Starting NetworkPlayerScript");
-        photonView.RPC("ChatMessage", PhotonTargets.All, "jup", "and jup!");
+        photonView.RPC("ChatMessage", PhotonTargets.Others, PhotonNetwork.player.NickName, "Hello!");
+        //photonView.RPC("ChatMessage", PhotonTargets.All, PhotonNetwork.player.NickName, "Hello2!");
     }
 
     void Update() {
@@ -15,7 +18,8 @@ public class NetworkPlayerScript : Photon.MonoBehaviour/*, IPunObservable*/ {
 
     [PunRPC]
     void ChatMessage(string a, string b) {
-        Debug.Log(string.Format("ChatMessage {0} {1}", a, b));
+        Debug.Log(string.Format("ChatMessage (ID: {0}) {1} {2} {3}", PhotonNetwork.player.ID, a, b, msgNum));
+        msgNum++;
     }
 
     //public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
